c7829913c2550b2baed50021eba34e582340dcc2
Alex Strashny <kpr9@cdc.gov>
2023-03-12 17:31:45 -0400

    rcc

---
+++ R/ed2020.R
@@ -0,0 +1,13 @@
+#' National Hospital Ambulatory Medical Care Survey (NHAMCS) Emergency Department (ED) 2020 Public Use File (PUF)
+#'
+#' A data system of visits to emergency departments. Note that the unit of
+#' observation is visits, not patients – this distinction is important since a single
+#' patient can make multiple visits.
+#'
+#' @source
+#' * SAS data: <https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Dataset_Documentation/NHAMCS/sas/ed2020_sas.zip>
+#' * Survey design variables: <https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Dataset_Documentation/NHAMCS/sas/readme2020-ed-sas.txt>
+#' * SAS formats: <https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Dataset_Documentation/NHAMCS/sas/ed20for.txt>
+#' * Documentation: <https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Dataset_Documentation/NHAMCS/doc20-ed-508.pdf>
+#' * Summary Tables: <https://www.cdc.gov/nchs/data/nhamcs/web_tables/2020-nhamcs-ed-web-tables-508.pdf>
+"ed2020"
---
+++ R/rcc2018.R
@@ -0,0 +1,9 @@
+#' National Study of Long-Term Care Providers (NSLTCP) Residential Care Community (RCC) 2018 Public Use File (PUF)
+#'
+#' A data system of RCCs.
+#'
+#' @source
+#' * SAS data: <https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/NPALS/final2018rcc_puf.sas7bdat>
+#' * Documentation: <https://www.cdc.gov/nchs/data/npals/2018NSLTCP-RCC-PUF-ReadMeFile_provider.pdf>
+#' * Codebook: <https://www.cdc.gov/nchs/data/npals/final2018rcc_puf_codebook.pdf>
+"rcc2018"

19431b487a409949d881ae066d1239cf576fc607
Alex Strashny <kpr9@cdc.gov>
2023-08-10 15:53:23 -0400

    surveytable

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach = function(libname, pkgname) {
   packageStartupMessage("\nThere are 3 related packages:"
-          , "\n* prettysurvey: functions for tabulating survey estimates"
+          , "\n* surveytable: functions for tabulating survey estimates"
           , "\n* nchsdata: public use files (PUFs) from the the National Center for Health Statistics (NCHS)"
           , "\n* importsurvey: functions for importing data into R"
           , "\n\nYou've just loaded ", pkgname, "."

3738fc6486055f89fbe77855b037742d77143b6e
Alex Strashny <kpr9@cdc.gov>
2023-08-11 01:18:17 -0400

    NHANES

---
+++ R/nhanes2016s.R
@@ -0,0 +1,13 @@
+#' National Health and Nutrition Examination Survey (NHANES) 2013-2016 Public Use File (PUF) (select variables and observations)
+#'
+#' A data system of the health and nutritional status of adults and children across the United States. The variables and observations selected are those used in the following report: Brody DJ, Pratt LA, Hughes JP. Prevalence of Depression Among Adults Aged 20 and Over: United States, 2013-2016. NCHS Data Brief. No 303. Hyattsville, MD: National Center for Health Statistics. 2018. <https://www.cdc.gov/nchs/products/databriefs/db303.htm>
+#'
+#' @source
+#' * SAS Transport Files / Demographic (DEMO) (2013-2014): <https://wwwn.cdc.gov/nchs/nhanes/2013-2014/DEMO_H.XPT>
+#' * SAS Transport Files / Demographic (DEMO) (2015-2016): <https://wwwn.cdc.gov/nchs/nhanes/2015-2016/DEMO_I.XPT>
+#' * SAS Transport Files / Mental Health - Depression Screener (DPQ) (2013-2014): <http://wwwn.cdc.gov/nchs/nhanes/2013-2014/DPQ_H.XPT>
+#' * SAS Transport Files / Mental Health - Depression Screener (DPQ) (2015-2016): <http://wwwn.cdc.gov/nchs/nhanes/2015-2016/DPQ_I.XPT>
+#' * Documentation: <https://wwwn.cdc.gov/nchs/nhanes/>
+#' * "Prevalence of Depression Among Adults Aged 20 and Over: United States, 2013-2016": <https://www.cdc.gov/nchs/products/databriefs/db303.htm>
+#' * Example code for the above report: <https://wwwn.cdc.gov/nchs/data/Tutorials/Code/DB303_R.r>
+"nhanes2016s"
